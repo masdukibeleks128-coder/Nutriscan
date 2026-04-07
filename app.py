f96b7aa364e6ee2310ac2e92adb7d46a829bdc53
masdukibeleks128-coder <masdukibeleks128@gmail.com>
2026-04-07 15:19:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.markdown("""
   @media (max-width: 480px) {
     .hero { min-height: 400px; padding-top: 16px; }
     .hero__overlay {
-      background: rgba(0,0,0,0.75) 40% !important;
+      background: rgba(0,0,0,0.45) 40% !important;
     }
     .hero__content { padding: 0 16px; max-width: 100%; }
     .hero__content h1 { font-size: 26px; margin-left: 5px; text-shadow: 2px 2px 4px rgba(0,0,0,0.8);}
