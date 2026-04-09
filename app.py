302ecc49255357a21e57edfef57b2feb4d000ae2
masdukibeleks128-coder <masdukibeleks128@gmail.com>
2026-04-09 12:41:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ hero_html = """
   }
   .hero {
     position: relative;
-    background-image: url('https://raw.githubusercontent.com/masdukibeleks128-coder/Nutriscan/main/background.jpeg');
+    background-image: url('https://raw.githubusercontent.com/masdukibeleks128-coder/Nutriscan/main/background2.jpeg');
     background-size: cover;
     background-position: center;
     border-radius: 12px;
